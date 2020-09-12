1fbb2463e90c044ccf4f635a33b67504ad454d6c
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-12 14:47:57 -0600

    Create image facade ImageFacade takes information from both serializers to make an array of image objects
    Co-Authored-By: Jane Greene <janegreenecasa@gmail.com>
    Co-Authored-By: Dan Sehl <dtsehl@users.noreply.github.com>
    Co-Authored-By: Rion <sciencefixion@users.noreply.github.com>

---
+++ facades/image_facade.rb
@@ -0,0 +1,32 @@
+Dotenv.load
+
+class ImageFacade
+  def self.images(keywords)
+    images = []
+    images << deep_image(keywords)
+    images << pixabay_image(keywords)
+    images
+  end
+
+  private
+
+  def self.deep_image(keywords)
+    data = DeepService.get_image(keywords)
+    image_data = {
+      id: data[:id],
+      url: data[:output_url],
+      source: 'Deep AI'
+    }
+    Image.new(image_data)
+  end
+
+  def self.pixabay_image(keywords)
+    data = PixabayService.get_image(keywords)
+    image_data = {
+      id: data[:id],
+      url: data[:webformatURL],
+      source: 'Pixabay'
+    }
+    Image.new(image_data)
+  end
+end
