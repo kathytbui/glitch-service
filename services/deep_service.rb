36f775b6e452adfd3920a8cb57cb70a1b3b3883d
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-12 14:46:29 -0600

    Create services to make api calls DeepServices makes api calls to DeepAI api PixabayService makes calls to pixabay api
    Co-Authored-By: Jane Greene <janegreenecasa@gmail.com>
    Co-Authored-By: Dan Sehl <dtsehl@users.noreply.github.com>
    Co-Authored-By: Rion <sciencefixion@users.noreply.github.com>

---
+++ services/deep_service.rb
@@ -0,0 +1,18 @@
+Dotenv.load
+
+class DeepService
+  def self.get_image(keywords)
+    response = conn.post '/api/text2img' do |req|
+      req.params['text'] = keywords
+    end
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  def self.conn
+    Faraday.new 'https://api.deepai.org' do |f|
+      f.headers['api-key'] = ENV['DEEP_API_KEY']
+    end
+  end
+end
---
+++ services/pixabay_service.rb
@@ -0,0 +1,17 @@
+Dotenv.load
+
+class PixabayService
+  def self.get_image(keywords)
+    json = JSON.parse(response(keywords).body, symbolize_names: true)
+    json[:hits][0]
+  end
+
+  private
+
+  def self.response(keywords)
+    Faraday.get "https://pixabay.com/api/" do |f|
+      f.params['key'] = ENV['PIXABAY_API_KEY']
+      f.params['q'] = keywords
+    end
+  end
+end
