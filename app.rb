1bdc0eebac6e51f5deab22c6d5e56e6b7217c01e
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-14 09:45:55 -0600

    Add require files to app.rb

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
-require "./spec/test_helper"
+# require "./spec/test_helper"
+require 'sinatra/base'
+require 'faraday'
+require 'rake'
+require 'shotgun'
+require 'dotenv'
+require 'pry'
+require 'json'
+require 'fast_jsonapi'
+require 'faraday'
+require 'rack/test'
+require './poros/image'
+require './services/pixabay_service'
+require './services/deep_service'
+require './facades/image_facade'
+require './serializers/image_serializer'
+require './app.rb'
+Dotenv.load
 
 class GlitchService < Sinatra::Base
   before do
